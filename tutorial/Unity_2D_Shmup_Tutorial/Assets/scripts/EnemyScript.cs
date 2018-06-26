0e566bc3e95e78a3965c914184895b6cab699818
StevenMeiklejohn <stevenmeiklejohn700@gmail.com>
2018-06-26 14:55:32 +0100

    Enemy sprite now autofires red bullet. Bullet is generated at appropriate position on enemy body.

---
+++ tutorial/Unity_2D_Shmup_Tutorial/Assets/scripts/EnemyScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour {
+
+  private WeaponScript[] weapons;
+
+  void Awake(){
+    weapons = GetComponentsInChildren<WeaponScript>();
+  }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    foreach (WeaponScript weapon in weapons){
+    // Auto-fire
+    if (weapon != null && weapon.CanAttack){
+      weapon.Attack(true);
+    }
+	}
+}
+}
+
